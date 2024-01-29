90b15b558b14ca234456df9762507f7b4e78caa7
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-01-29 14:08:23 -0500

    changed to multi source

--- modules/utils.py
+++ modules/utils.py
@@ -1,17 +1,14 @@
 import torch
 
 def pad_seqs(seqs, num_chan, num_pad=100):
-    if num_pad != 0:
-        pad = torch.zeros(num_chan, num_pad)
-        pad_seqs = [torch.cat([pad, x, pad], dim=1) for x in seqs]
-        return torch.stack(pad_seqs, dim=0)
-    else:
-        return torch.stack(seqs, dim=1)
+    pad = torch.zeros(num_chan, num_pad)
+    pad_seqs = [torch.cat([pad, x, pad], dim=1) for x in seqs]
+    return torch.stack(pad_seqs, dim=0)
 
 def utility_fct(Xy):
     X, y = zip(*Xy)
     X = pad_seqs(X, 2, num_pad=100)
-    y = pad_seqs(y, 2, num_pad=0)
+    y = torch.stack(y, dim=0)
     return (X, y)
 
 def transform(x):
