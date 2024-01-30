99c37df8d3e360a6cd84aa7310ae214332a8b3af
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-01-30 15:14:48 -0500

    rm batch_norm and add tanh

--- modules/model.py
+++ modules/model.py
@@ -96,12 +96,12 @@ class ssnet(nn.Module):
         block = nn.Sequential(
             nn.Conv1d(in_channels, in_channels, kernel_size=k, groups=in_channels, padding='same'),
             nn.Conv1d(in_channels, out_channels, kernel_size=1, padding='same'),
-            nn.GELU(),
-            nn.BatchNorm1d(out_channels),
+            nn.Tanh(),
+            #nn.BatchNorm1d(out_channels),
             nn.Conv1d(out_channels, out_channels, kernel_size=k, groups=out_channels, padding='same'),
             nn.Conv1d(out_channels, out_channels, kernel_size=1, padding='same'),
-            nn.GELU(),
-            nn.BatchNorm1d(out_channels),
+            nn.Tanh(),
+            #nn.BatchNorm1d(out_channels),
         )
         return block
 
@@ -109,7 +109,7 @@ class ssnet(nn.Module):
     def final_block(in_channels, out_channels, k=1):
         block = nn.Sequential(
             nn.Conv1d(in_channels, out_channels, kernel_size=k, padding='same'),
-            nn.GELU(),
-            nn.BatchNorm1d(out_channels),
+            nn.Tanh(),
+            #nn.BatchNorm1d(out_channels),
         )
         return block
