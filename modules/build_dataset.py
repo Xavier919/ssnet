5b1a4544229c9b9fb0d7385579f399783c1e3b8f
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-01-31 11:42:26 -0500

    modif audio stack

--- modules/build_dataset.py
+++ modules/build_dataset.py
@@ -4,7 +4,7 @@ import numpy as np
 from utils import transform
 
 
-def process_audio(mus, tag, frame_length=50000):
+def process_audio(mus, tag, frame_length=30000):
     """
     Processes an audio dataset to extract frames of audio and corresponding targets.
 
@@ -18,11 +18,7 @@ def process_audio(mus, tag, frame_length=50000):
     for track in mus:
         # Get the mixture audio and the target stem
         mixture_audio = track.stems[0].T
-        t1 = track.stems[1].T
-        t2 = track.stems[2].T
-        t3 = track.stems[3].T
-        t4 = track.stems[4].T
-        target = np.stack([t1,t2,t3,t4])
+        target = np.stack([track.stems[i].T for i in range(1, 5)])
 
         # Iterate over the audio in chunks of 'frame_length'
         for start_idx in range(0, mixture_audio.shape[1] - frame_length + 1, frame_length):
