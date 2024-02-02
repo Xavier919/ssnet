e740b6fe93999dfaef76bceb17037a4cee782efe
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-02-02 16:37:20 -0500

    changed snr to sdr

--- modules/train_script.py
+++ modules/train_script.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     
     #loss_function = PermutationInvariantTraining(scale_invariant_signal_noise_ratio, mode="permutation-wise", eval_func="max").to(device)
     
-    loss_function = PermutationInvariantTraining(MSELoss_, mode="permutation-wise", eval_func="max").to(device)
+    loss_function = PermutationInvariantTraining(MSELoss_, mode="speaker-wise", eval_func="max").to(device)
 
 
     start_time = time.time()
