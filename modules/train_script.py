54ff9ab1bbf7bf3a01f7b2504db7eab11cb8a686
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-02-07 10:04:37 -0500

    mse loss

--- modules/train_script.py
+++ modules/train_script.py
@@ -60,12 +60,12 @@ if __name__ == "__main__":
     valid_loader = DataLoader(valid_set, collate_fn=utility_fct, batch_size=args.batch_size, num_workers=8, shuffle=True)
 
     optimizer = optim.Adam(ssnet_.parameters(),lr=args.lr, weight_decay=args.l2)
-    mse = MSELoss(reduction='mean').to_device()
-    sdr = SignalDistortionRatio().to_device()
+    mse = MSELoss(reduction='mean')
+    sdr = SignalDistortionRatio()
 
     #For sdr, objective is to maximize, whereas for mse objective is to minimize
 
-    loss_function = PermutationInvariantTraining(sdr,mode="speaker-wise", eval_func="max")
+    loss_function = PermutationInvariantTraining(sdr,mode="speaker-wise", eval_func="max").to(device)
     #loss_function = PermutationInvariantTraining(mse,mode="speaker-wise", eval_func="min")
 
 
